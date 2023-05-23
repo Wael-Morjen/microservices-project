57e77c97652482e63698e3d19cdd8299833424fc
wael-morjen <waelmorjen@gmail.com>
2023-05-23 18:34:09 +0100

    fixed

--- patient/models.js
+++ patient/models.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
-// connect to the DB
-const db = new sqlite3.Database('./patients.sqlite', (err) => {
+// Connect to the database
+const db = new sqlite3.Database('./patients.sqlite', err => {
   if (err) {
     console.error(err.message);
     throw err;
@@ -9,6 +9,7 @@ const db = new sqlite3.Database('./patients.sqlite', (err) => {
   console.log('Database connected.');
 });
 
+// Create the patients table if it doesn't exist
 db.run(`CREATE TABLE IF NOT EXISTS patients (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
@@ -16,7 +17,7 @@ db.run(`CREATE TABLE IF NOT EXISTS patients (
   phone NUMBER NOT NULL UNIQUE
 )`);
 
-
+// Define the Patient class
 class Patient {
   constructor(name, lastName, phone) {
     this.name = name;
@@ -24,71 +25,79 @@ class Patient {
     this.phone = phone;
   }
 
+  // Save a new patient to the database
+  save(callback) {
+    db.run(
+      `INSERT INTO patients (name, lastName, phone) VALUES (?, ?, ?)`,
+      [this.name, this.lastName, this.phone],
+      function(err) {
+        if (err) {
+          console.error(err.message);
+          return callback(err);
+        }
+        console.log(`patient ${this.name} added with the ID: ${this.lastID}`);
+        callback(null, this.lastID);
+      }
+    );
+  }
 
-    // Enregistrer un nouvel client dans la base de données
-    save(callback) {
-    db.run(`INSERT INTO patients (name, lastName, phone) VALUES (?, ?, ?)`,
-    [this.name, this.lastName, this.phone], function(err) {
-    if (err) {
-    console.error(err.message);
-    return callback(err);
-    }
-    console.log(`patient ${this.name} added with the ID: ${this.lastID}`);
-    callback(null, this.lastID);
-    });
-    }
-
-    // Rechercher tous les clients dans la base de données
-    static findAll(callback) {
+  // Find all patients in the database and return them as Patient objects
+  static findAll(callback) {
     db.all(`SELECT * FROM patients`, [], function(err, rows) {
-    if (err) {
-    console.error(err.message);
-    return callback(err);
-    }
-    const patients = rows.map(row => new Patient(row.name, row.lastName,
-    row.phone));
-    callback(null, patients);
+      if (err) {
+        console.error(err.message);
+        return callback(err);
+      }
+      const patients = rows.map(
+        row => new Patient(row.name, row.lastName, row.phone)
+      );
+      callback(null, patients);
     });
-    }
-    
-    
-    // Rechercher un client par ID dans la base de données
-    static findById(id, callback) {
-    db.get(`SELECT * FROM patients WHERE id = ?`, [id], function(err, row) {
-    if (err) {
-    console.error(err.message);
-    return callback(err);
-    }
-    if (!row) {
-    return callback(new Error('patient not found'));
-    }
-    const patient = new Patient(row.name, row.lastName, row.phone);
-    callback(null, patient);
-    });
-    }
+  }
 
-    // Mettre à jour un client dans la base de données
-    updateById(id, name, lastName, phone, callback) {
-    db.run(`UPDATE patients SET name = ?, lastName = ?, phone = ? WHERE id = ?`,
-    [name, lastName, phone, id], function(err) {
-    if (err) {
-    console.error(err.message);
-    return callback(err);
-    }
-    console.log(`patient with the ID: ${id} has been updated.`);
-    callback(null);
+  // Find a patient by ID and return it as a Patient object
+  static findById(id, callback) {
+    db.get(`SELECT * FROM patients WHERE id = ?`, [id], function(err, row) {
+      if (err) {
+        console.error(err.message);
+        return callback(err);
+      }
+      if (!row) {
+        return callback(new Error('patient not found'));
+      }
+      const patient = new Patient(row.name, row.lastName, row.phone);
+      callback(null, patient);
     });
-    }
-    // Supprimer un client de la base de données
-    deleteById(id, callback) {
+  }
+
+  // Update a patient by ID with the provided information
+  updateById(id, name, lastName, phone, callback) {
+    db.run(
+      `UPDATE patients SET name = ?, lastName = ?, phone = ? WHERE id = ?`,
+      [name, lastName, phone, id],
+      function(err) {
+        if (err) {
+          console.error(err.message);
+          return callback(err);
+        }
+        console.log(`patient with the ID: ${id} has been updated.`);
+        callback(null);
+      }
+    );
+  }
+
+  // Delete a patient by ID
+  deleteById(id, callback) {
     db.run(`DELETE FROM patients WHERE id = ?`, [id], function(err) {
-    if (err) {
-    console.error(err.message);
-    return callback(err);
-    }
-    console.log(`the patient with the ID: ${id} has been deleted`);
-    callback(null);
+      if (err) {
+        console.error(err.message);
+        return callback(err);
+      }
+      console.log(`the patient with the ID: ${id} has been deleted`);
+      callback(null);
     });
-    }
+  }
 }
-    module.exports = db;
+
+// Export the database object for use in other parts of the application
+module.exports = db;
